f2cf3f54483df5c3337d6b72cad9fcf60e481e0e
Jay Nelson <jay@duomark.com>
2014-03-16 20:19:30 -0700

    Restructured for readability and removal of magic constants
    The entire source file except for test code was rewritten to:
    
      1) Add semantic macros for magic integer constants
      2) Avoid repeating the internal binary definitions (DRY)
      3) Use << ?MACROS >> to make binary structures readable
      4) Consistently use /unsigned for pointers and sizes
      5) Test all terminal cases in recursive functions last
      6) Add log functions for reporting binary dictionaries
      7) Add versioning for the internal binary format

--- src/vbisect.erl
+++ src/vbisect.erl
@@ -1,86 +1,154 @@
-
-
+%% ----------------------------------------------------------------------------
+%%
+%% vbisect: Variable Binary Dictionary data structure
+%%
+%% Copyright 2012-2014 (c) Trifork A/S.  All Rights Reserved.
+%% http://trifork.com/ info@trifork.com
+%%
+%% This file is provided to you under the Apache License, Version 2.0 (the
+%% "License"); you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+%% License for the specific language governing permissions and limitations
+%% under the License.
+%%
+%% ----------------------------------------------------------------------------
 -module(vbisect).
+-author('Kresten Krab Thorup <krab@trifork.com>').
 
--export([from_orddict/1, from_gb_tree/1, to_gb_tree/1, find/2, find_geq/2, foldl/3, foldr/3, to_orddict/1, merge/3]).
+-export([
+         from_orddict/1, to_orddict/1,
+         from_gb_tree/1, to_gb_tree/1,
+         find/2, find_geq/2,
+         foldl/3, foldr/3, merge/3,
+         dictionary_size_in_bytes/1,
+         log_summary/1, log_detail/1, log_full/1
+        ]).
 
--define(MAGIC, "vbis").
--type key() :: binary().
--type value() :: binary().
+-type key()     :: binary().
+-type value()   :: binary().
 -type bindict() :: binary().
+-export_type([key/0, value/0, bindict/0]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--spec from_gb_tree(gb_tree()) -> bindict().
-from_gb_tree({Count,Node}) when Count =< 16#ffffffff ->
-    {_BinSize,IOList} = encode_gb_node(Node),
-    erlang:iolist_to_binary([ <<?MAGIC,  Count:32/unsigned >> | IOList ]).
+%% Vbisect macros contain only the comma-delimited binary
+%% matching fields, so that in context the << ... >> are
+%% required to make clear that a binary pattern-match occurs.
 
-encode_gb_node({Key, Value, Smaller, Bigger}) when is_binary(Key), is_binary(Value) ->
-    {BinSizeSmaller, IOSmaller} = encode_gb_node(Smaller),
-    {BinSizeBigger, IOBigger} = encode_gb_node(Bigger),
+%% Magic prefix identifies Vbisect type, and allows up to
+%% 255 versions of the implementation co-resident.
+-define(V1, 1).
+-define(MAGIC_V1, "vbs", ?V1:8/unsigned).
+-define(MAX_DICT_ENTRIES, 16#ffffffff).
 
-    KeySize = byte_size(Key),
-    ValueSize = byte_size(Value),
-    { 2 + KeySize
-      + 4 + ValueSize
-      + 4 + BinSizeSmaller
-      + BinSizeBigger,
+%% Fixed-size fields for computing positions in bytes.
+-define(KEY_SIZE_IN_BYTES,        2).
+-define(VALUE_SIZE_IN_BYTES,      4).
+-define(DICT_PTR_SIZE_IN_BYTES,   4).
 
-      [ << KeySize:16, Key/binary,
-           BinSizeSmaller:32 >>, IOSmaller,
-        << ValueSize:32, Value/binary >> | IOBigger ] };
+%% Fixed-size fields for constructing / destructuring binaries.
+-define(KEY_SIZE_IN_BITS,        16).
+-define(VALUE_SIZE_IN_BITS,      32).
+-define(DICT_PTR_SIZE_IN_BITS,   32).
+-define(DICT_ENTRIES_COUNT_BITS, 32).
 
-encode_gb_node(_) ->
-    { 0, [] }.
+%% Match or construct only the Vbisect header fields...
+-define(MAKE_VBISECT_HDR(__Num_Entries),
+        ?MAGIC_V1, __Num_Entries:?DICT_ENTRIES_COUNT_BITS/unsigned ).
 
-to_gb_tree(<<?MAGIC,  Count:32, Nodes/binary >>) ->
-    { Count, to_gb_node(Nodes) }.
+%% Match to get the number of entries and the raw binary dictionary...
+-define(MATCH_VBISECT_DATA(__Num_Entries, __Vbisect),
+        ?MAKE_VBISECT_HDR(__Num_Entries), __Vbisect/binary ).
 
-to_gb_node( <<>> ) ->
-    nil;
+%% All fields inside the dictionary have a size preceding the actual value...
+-define(KEY_ENTRY(__Key),
+        __KeySize:?KEY_SIZE_IN_BITS/unsigned, __Key:__KeySize/binary).
 
-to_gb_node( << KeySize:16, Key:KeySize/binary,
-               BinSizeSmaller:32, Smaller:BinSizeSmaller/binary,
-               ValueSize:32, Value:ValueSize/binary,
-               Bigger/binary >> ) ->
-    {Key, Value,
-     to_gb_node(Smaller),
-     to_gb_node(Bigger)}.
+-define(MAKE_KEY_ENTRY(__KeySize, __Key),
+        __KeySize:?KEY_SIZE_IN_BITS/unsigned, __Key:__KeySize/binary).
 
--spec find(Key::key(), Dict::bindict()) ->
-                  { ok, value() } | error.
-find(Key, <<?MAGIC, _:32, Binary/binary>>) ->
-    find_node(byte_size(Key), Key, Binary).
+-define(VALUE_ENTRY(__Value),
+        __ValueSize:?VALUE_SIZE_IN_BITS/unsigned, __Value:__ValueSize/binary).
 
-find_node(KeySize, Key, <<HereKeySize:16, HereKey:HereKeySize/binary,
-                          BinSizeSmaller:32, _:BinSizeSmaller/binary,
-                          ValueSize:32, Value:ValueSize/binary,
-                          _/binary>> = Bin) ->
-    if
-        Key < HereKey ->
-            Skip = 6 + HereKeySize,
-            << _:Skip/binary, Smaller:BinSizeSmaller/binary, _/binary>> = Bin,
-            find_node(KeySize, Key, Smaller);
-        HereKey < Key ->
-            Skip = 10 + HereKeySize + BinSizeSmaller + ValueSize,
-            << _:Skip/binary, Bigger/binary>> = Bin,
-            find_node(KeySize, Key, Bigger);
-        true ->
-            {ok, Value}
-    end;
+-define(MAKE_VALUE_ENTRY(__ValueSize, __Value),
+        __ValueSize:?VALUE_SIZE_IN_BITS/unsigned, __Value:__ValueSize/binary).
 
-find_node(_, _, <<>>) ->
-    error.
+-define(SMALLER_ENTRY(__Smaller),
+        __BinSizeSmaller:?DICT_PTR_SIZE_IN_BITS/unsigned, __Smaller:__BinSizeSmaller/binary).
+
+%% Except Bigger which falls last and thus consumes the rest of the binary.
+-define(BIGGER_ENTRY(__Bigger), __Bigger/binary).
+
+%% Deconstruct the raw binary dictionary which is formatted as
+%% << Key, Smaller_Node, Value, Bigger_Node >> so that CPU cache
+%% lines are pre-fetched when search descends SmallerNode. Also
+%% note that Bigger is last and therefore does not need to waste
+%% space storing a size field.
+-define(MATCH_VBISECT_NODE(__Key, __Value, __Smaller, __Bigger),
+        ?KEY_ENTRY     (__Key),
+        ?SMALLER_ENTRY (__Smaller),
+        ?VALUE_ENTRY   (__Value),
+        ?BIGGER_ENTRY  (__Bigger)).
+
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+-spec from_orddict(orddict:orddict()) -> bindict().
+-spec to_orddict(bindict()) -> orddict:orddict().
+
+from_orddict(OrdDict) ->
+    from_gb_tree(gb_trees:from_orddict(OrdDict)).
 
 to_orddict(BinDict) ->
-    foldr(fun(Key,Value,Acc) ->
-                  [{Key,Value}|Acc]
-          end,
-          [],
-          BinDict).
+    Fold_Fn = fun(Key, Value, Acc) -> [{Key, Value} | Acc] end,
+    foldr(Fold_Fn, [], BinDict).
+
+
+-spec from_gb_tree(gb_tree()) -> bindict().
+-spec to_gb_tree  (bindict()) -> gb_tree().
+
+from_gb_tree({Num_Entries, Node}) when Num_Entries =< ?MAX_DICT_ENTRIES ->
+    {_BinSize, IOList} = encode_gb_node(Node),
+    iolist_to_binary([ << ?MAKE_VBISECT_HDR(Num_Entries) >> | IOList ]).
+
+to_gb_tree(<< ?MATCH_VBISECT_DATA(Num_Entries, Nodes) >> = _BinDict) ->
+    {Num_Entries, to_gb_node(Nodes)}.
+
+
+-spec find    (key(), bindict()) -> {ok, value()}                    | error.
+-spec find_geq(key(), bindict()) -> {ok, Key::key(), Value::value()} | none.
+
+find(Key, << ?MATCH_VBISECT_DATA(_Num_Entries, Nodes) >> = _BinDict) ->
+    find_node(Key, Nodes).
+
+%% Find largest Key + Value smaller than or equal to Key.
+%% This is good for an inner node where key is the smallest key in the child node.
+find_geq(Key, << ?MATCH_VBISECT_DATA(_Num_Entries, Nodes) >> = _BinDict) ->
+    find_geq_node(Key, none, Nodes).
+
+
+-type dict_fold_fn()  :: fun((Key::key(), Value::value(),     Acc::term()) -> term()).
+-type dict_merge_fn() :: fun((Key::key(), Value1::value(), Value2::term()) -> term()).
+
+-spec foldl(dict_fold_fn(),  term(),    bindict()) -> term().
+-spec foldr(dict_fold_fn(),  term(),    bindict()) -> term().
+-spec merge(dict_merge_fn(), bindict(), bindict()) -> bindict().
+
+foldl(Fun, Acc, << ?MATCH_VBISECT_DATA(_Num_Entries, Nodes) >> = _BinDict) ->
+    foldl_node(Fun, Acc, Nodes).
+
+foldr(Fun, Acc, << ?MATCH_VBISECT_DATA(_Num_Entries, Nodes) >> = _BinDict) ->
+    foldr_node(Fun, Acc, Nodes).
 
 merge(Fun, BinDict1, BinDict2) ->
     OD1 = to_orddict(BinDict1),
@@ -88,76 +156,138 @@ merge(Fun, BinDict1, BinDict2) ->
     OD3 = orddict:merge(Fun, OD1, OD2),
     from_orddict(OD3).
 
-%% @doc Find largest KV smaller than or equal to key.
-%% This is good for an inner node where key is the smallest key
-%% in the child node.
 
--spec find_geq(Key::binary(), Binary::binary()) ->
-                      none | {ok, Key::key(), Value::value()}.
+-spec dictionary_size_in_bytes(bindict()) -> pos_integer().
 
-find_geq(Key, <<?MAGIC, _:32, Binary/binary>>) ->
-    find_geq_node(byte_size(Key), Key, Binary, none).
+dictionary_size_in_bytes(BinDict) ->
+    byte_size(BinDict).
 
-find_geq_node(_, _, <<>>, Else) ->
-    Else;
+%% Functions for logging information about vbisect instances.
+%% They attempt to organize data in lines but leave caller to insert newlines.
+-spec log_summary(bindict()) -> iolist().
+-spec log_detail (bindict()) -> iolist().
+-spec log_full   (bindict()) -> iolist().
 
-find_geq_node(KeySize, Key, <<HereKeySize:16, HereKey:HereKeySize/binary,
-                              BinSizeSmaller:32, _:BinSizeSmaller/binary,
-                              ValueSize:32, Value:ValueSize/binary,
-                              _/binary>> = Bin, Else) ->
-    if
-        HereKey > Key ->
-            if Else =:= none ->
-                    Skip = 6 + HereKeySize,
-                    << _:Skip/binary, Smaller:BinSizeSmaller/binary, _/binary>> = Bin,
-                    find_geq_node(KeySize, Key, Smaller, Else);
-               true ->
-                    Else
-            end;
-        HereKey < Key ->
-            Skip = 10 + HereKeySize + BinSizeSmaller + ValueSize,
-            << _:Skip/binary, Bigger/binary>> = Bin,
-            find_geq_node(KeySize, Key, Bigger, {ok, HereKey, Value});
-        true ->
-            {ok, HereKey, Value}
-    end.
+%% Display the number of entries and size of the dictionary in bytes.
+log_summary(<< ?MATCH_VBISECT_DATA(Num_Entries, _Nodes) >> = BinDict) ->
+     case Num_Entries of
+         1 -> [<<"VBS1: 1 entry">>];
+         _ -> [<<"VBS1: ">>, integer_to_binary(Num_Entries), <<" entries">>]
+     end
+        ++ [<<" (">>, integer_to_binary(dictionary_size_in_bytes(BinDict)), <<" bytes)">>].
 
--spec foldl(fun((Key::key(), Value::value(), Acc::term()) -> term()), term(), bindict()) ->
-                   term().
-foldl(Fun, Acc, <<?MAGIC, _:32, Binary/binary>>) ->
-    foldl_node(Fun, Acc, Binary).
+%% Display summary plus the midpoint key / value node.
+log_detail(<< ?MATCH_VBISECT_DATA(_Num_Entries, Nodes) >> = Bin_Dict) ->
+    Summary  = iolist_to_binary(log_summary(Bin_Dict)),
+    Midpoint = iolist_to_binary(
+                 case Nodes of
+                     <<>> -> "";
+                     << ?MATCH_VBISECT_NODE(Key, Value, _Smaller, _Bigger) >> ->
+                         [<<"Midpoint: ">>, <<"{">>, Key, <<", ">>, Value, <<"}">>]
+                 end),
+    [Summary, Midpoint].
 
-foldl_node(_Fun, Acc, <<>>) ->
-    Acc;
+%% Display summary plus the full dictionary of keys and values in sorted order.
+log_full(Bin_Dict) ->
+    Summary = iolist_to_binary(log_summary(Bin_Dict)),
+    Values  = orddict:to_list(to_orddict(Bin_Dict)),
+    [Summary | Values].
 
-foldl_node(Fun, Acc, <<KeySize:16, Key:KeySize/binary,
-                       BinSizeSmaller:32, Smaller:BinSizeSmaller/binary,
-                       ValueSize:32, Value:ValueSize/binary,
-                       Bigger/binary>>) ->
+
+%% ===================================================================
+%% Support functions
+%% ===================================================================
+
+%% Size of a binary node while constructing it from elements.
+compute_node_size_in_bytes(KeySize, ValueSize, BinSizeSmaller, BinSizeBigger) ->
+    ?KEY_SIZE_IN_BYTES + KeySize + ?VALUE_SIZE_IN_BYTES + ValueSize
+        + ?DICT_PTR_SIZE_IN_BYTES + BinSizeSmaller + BinSizeBigger.
+    
+skip_to_bigger_node(Key, Value, Smaller) ->
+    ?KEY_SIZE_IN_BYTES + byte_size(Key) + ?VALUE_SIZE_IN_BYTES + byte_size(Value)
+        + ?DICT_PTR_SIZE_IN_BYTES + byte_size(Smaller).
+
+%% Recursively encode gb_trees format as a binary tree.
+encode_gb_node({Key, Value, Smaller, Bigger}) when is_binary(Key), is_binary(Value) ->
+    {BinSizeSmaller, IOSmaller} = encode_gb_node(Smaller),
+    {BinSizeBigger,  IOBigger}  = encode_gb_node(Bigger),
+
+    KeySize    = byte_size(Key),
+    ValueSize  = byte_size(Value),
+    NodeSize   = compute_node_size_in_bytes(KeySize, ValueSize, BinSizeSmaller, BinSizeBigger),
+    NodeBinary = [
+                  %% The Key in Size/Key format...
+                  << ?MAKE_KEY_ENTRY(KeySize, Key),
+
+                     %% The smaller node in Size/Smaller format...
+                     BinSizeSmaller:?DICT_PTR_SIZE_IN_BITS/unsigned >>, IOSmaller,
+
+                  %% The Value for the Key in Size/Value format...
+                  << ?MAKE_VALUE_ENTRY(ValueSize, Value) >>
+
+                      %% The Bigger node is optimized as remaining binary with no size.
+                      | IOBigger
+                 ],
+    {NodeSize, NodeBinary};
+encode_gb_node(nil) -> {0, []}.
+
+%% Convert from binary format to gb_trees [{Key, Value, Smaller, Bigger}, ...].
+to_gb_node(<< ?MATCH_VBISECT_NODE(Key, Value, Smaller, Bigger) >>) ->
+    {Key, Value, to_gb_node(Smaller), to_gb_node(Bigger)};
+to_gb_node(<<>>) -> nil.
+
+%% Recursively search nodes for a key, loading as little data to CPU cache as possible.
+%% Avoid creating a sub-binary for Bigger unless we must search the right subtree.
+find_node(Key, << ?MATCH_VBISECT_NODE(HereKey, Value, Smaller, _) >> = Node) ->
+    case HereKey of
+        Candidate when Key  <  Candidate -> find_node(Key, Smaller);
+        Candidate when Key  >  Candidate -> find_node_bigger(Key, Node, HereKey, Value, Smaller);
+        Candidate when Key =:= Candidate -> Value
+    end;
+find_node(_, <<>>) -> error.
+
+%% Keep the same arg order as find_node to avoid overhead.
+find_node_bigger(Key, Node, HereKey, Value, Smaller) ->
+    Skip_Size = skip_to_bigger_node(HereKey, Value, Smaller),
+    Bigger_Len = byte_size(Node) - Skip_Size,
+    find_node(Key, binary:part(Node, Skip_Size, Bigger_Len)).
+
+%% Recursively search nodes for greatest lesser key, loading as little data to CPU cache as possible.
+%% Avoid creating a sub-binary for Bigger unless we must search the right subtree.
+find_geq_node(Key, Else, << ?MATCH_VBISECT_NODE(HereKey, Value, Smaller, _) >> = Node) ->
+    case HereKey of
+        Candidate when Key  <  Candidate, Else =/= none -> Else;
+        Candidate when Key  <  Candidate -> find_geq_node(Key, Else, Smaller); 
+        Candidate when Key  >  Candidate -> find_geq_node_bigger(Key, {ok, HereKey, Value}, Node,
+                                                                 HereKey, Value, Smaller);
+        Candidate when Key =:= Candidate -> {HereKey, Value}
+    end;
+find_geq_node(_, Else, <<>>) -> Else.
+
+%% Keep the same arg order as find_geq_node to avoid overhead.
+find_geq_node_bigger(Key, Else, Node, HereKey, Value, Smaller) ->
+    Skip_Size = skip_to_bigger_node(HereKey, Value, Smaller),
+    Bigger_Len = byte_size(Node) - Skip_Size,
+    find_geq_node(Key, Else, binary:part(Node, Skip_Size, Bigger_Len)).
+
+
+foldl_node(Fun, Acc, << ?MATCH_VBISECT_NODE(Key, Value, Smaller, Bigger) >> ) ->
     Acc1 = foldl_node(Fun, Acc, Smaller),
     Acc2 = Fun(Key, Value, Acc1),
-    foldl_node(Fun, Acc2, Bigger).
+    foldl_node(Fun, Acc2, Bigger);
+foldl_node(_Fun, Acc, <<>>) -> Acc.
 
 
--spec foldr(fun((Key::key(), Value::value(), Acc::term()) -> term()), term(), bindict()) ->
-                   term().
-foldr(Fun, Acc, <<?MAGIC, _:32, Binary/binary>>) ->
-    foldr_node(Fun, Acc, Binary).
-
-foldr_node(_Fun, Acc, <<>>) ->
-    Acc;
-
-foldr_node(Fun, Acc, <<KeySize:16, Key:KeySize/binary,
-                       BinSizeSmaller:32, Smaller:BinSizeSmaller/binary,
-                       ValueSize:32, Value:ValueSize/binary,
-                       Bigger/binary>>) ->
+foldr_node(Fun, Acc, << ?MATCH_VBISECT_NODE(Key, Value, Smaller, Bigger) >> ) ->
     Acc1 = foldr_node(Fun, Acc, Bigger),
     Acc2 = Fun(Key, Value, Acc1),
-    foldr_node(Fun, Acc2, Smaller).
+    foldr_node(Fun, Acc2, Smaller);
+foldr_node(_Fun, Acc, <<>>) -> Acc.
 
 
-from_orddict(OrdDict) ->
-    from_gb_tree(gb_trees:from_orddict(OrdDict)).
+%% ===================================================================
+%% Test functions
+%% ===================================================================
 
 -ifdef(TEST).
 
